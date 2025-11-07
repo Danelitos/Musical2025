f94c0d8a91a8771ce36ed083014ba4faad0f85a1
Danel Alonso Egusquiza (Ingeteam RDanel.AlonsoD Europe) <Danel.Alonso@ingeteam.com>
2025-11-07 17:46:43 +0100

    [FEAT] Musical 2025 - Web del Musical En Belén de Judá terminada!

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  // Clave pública de Stripe LIVE para producción
   stripePublishableKey: 'pk_live_51RdBB9GSf4CQPQbWqIhWyIIJpsBbc6PlLvvyr69kNoGbs8XVaHJyq4XaWb4Jak4NuFGjLD8S3TXnZEuUhaXbRoAk00sHsWQ8Dx',
   apiUrl: 'https://enbelendejuda.com/api',
   appUrl: 'https://enbelendejuda.com'
